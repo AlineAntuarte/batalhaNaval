df18213c462da226b6f9334bcfd258cac87555c4
AlineAntuarte <aline.ba@aluno.ifsc.edu.br>
2026-01-15 22:58:57 -0300

    Navio

--- batalhaNaval/src/oceano.java
+++ batalhaNaval/src/oceano.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class oceano {
     public static void main(String[] args) throws Exception {
+
         /*
          * O Segredo: O "Game Loop" (O Ciclo do Jogo)
          * Diferente dos exercícios de aula que começam e terminam, um jogo funciona em
@@ -11,6 +13,9 @@ public class oceano {
          */
         // 1. Criei o tabuleiro
         char[][] tabuleiro = new char[5][5];
+        // 1.1 Criei um compartimento para os navios
+        char[][] navio = new char[1][1];
+
         // 2. Vou cobrir ele de "água" onde os navios ficam (~)
 
         /*
@@ -26,11 +31,12 @@ public class oceano {
 
         // 3. Exibição do tabuleiro na tela
         System.out.println("--- Batalha Naval ---");
+
         exibirTabuleiro(tabuleiro);
     }
 
     public static void exibirTabuleiro(char[][] mapa) {
-        //Aqui é onde foi "contruído" a ordem de exibir o tabuleiro
+        // Aqui é onde foi "contruído" a ordem de exibir o tabuleiro
         for (int linha = 0; linha < 5; linha++) {
             for (int coluna = 0; coluna < 5; coluna++) {
                 System.out.print(mapa[linha][coluna] + " ");
@@ -38,4 +44,51 @@ public class oceano {
             System.out.println(); // Um sout extra executado "vazio" para pular uma linha após imprimir 5 colunas
         }
     }
+
+    public static void criandoNavio(char[][] navio) {
+        Random gerador = new Random(); // Inicialize o gerador aqui
+
+        int naviol1 = 0;
+        int navioc1 = 0;
+        int naviol2 = 0;
+        int navioc2 = 0;
+        int naviol3 = 0;
+        int navioc3 = 0;
+
+        naviol1 = gerador.nextInt(5);
+        System.out.println(naviol1);
+
+        navioc1 = gerador.nextInt(5);
+        System.out.println(navioc1);
+
+        naviol2 = gerador.nextInt(5);
+        System.out.println(naviol2);
+
+        navioc2 = gerador.nextInt(5);
+        System.out.println(navioc2);
+
+        naviol3 = gerador.nextInt(5);
+        System.out.println(naviol3);
+
+        navioc3 = gerador.nextInt(5);
+        System.out.println(navioc3);
+
+        for (int linha = 0; linha < 5; linha++) {
+            for (int coluna = 0; coluna < 5; coluna++) {
+                navio[linha][coluna] = '~';
+            }
+        }
+
+        for (int i = 0; i < 2; i++) {
+            if (i == 0) {
+                // Linha
+                int linha = 0;
+
+            } else {
+                // Coluna
+            }
+        }
+
+    }
+
 }
